9e4c071589c72b4139cccc82a067a25c425c2156
AndreiNC05 <s190298@student.dtu.dk>
2019-10-27 17:19:51 +0100

    db connection

---
+++ Database.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Database {
+	private boolean connectDb(String usr,String passw) {
+	    final String user = "root";
+	    final String pass = "Password123!";
+	    final String connection = "jdbc:mysql://localhost:3306/DB";
+	    System.out.println(user);
+	    System.out.println(pass);
+
+	    try
+	    {
+	      Class.forName("com.mysql.jdbc.Driver");
+	      Connection con = DriverManager.getConnection(connection,user,pass);
+	      
+	      PreparedStatement statement = con.prepareStatement("insert into users (Username, Passord) values (admin,pass)");
+	      statement.executeUpdate();
+	      con.commit();
+	    }
+	    catch (Exception e)
+	    {
+	      System.err.println("Got an exception!");
+	      System.err.println(e.getMessage());
+	    }
+		return true;
+	}
+
+}
