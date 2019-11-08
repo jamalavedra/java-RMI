c10686cd822a4619162199b2348d92e4985bc1bb
s190699 <190699@student.dtu.dk>
2019-11-08 18:30:32 +0100

    clear .sql file

--- extraFiles/sql_database/data.sql
+++ extraFiles/sql_database/data.sql
@@ -1,30 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 13, 2018 at 04:36 PM
--- Server version: 5.7.23
--- PHP Version: 7.2.10
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `passwordlogins`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `data`
 --
 USE data_security;
